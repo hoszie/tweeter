08a913eb117415ef32973c4337af0edd2369a832
Nick <nhoszko@gmail.com>
2019-11-06 03:13:43 +0000

    Functioning class toggle when number goes below 0

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("textarea").on('keyup', function() {
+    $(this).parent().find("span").text(140 - $(this).val().length);
+    let $tweetLength = $(this).val().length;
+    if ($tweetLength > 140) {
+      $("span").addClass('negative');
+    } else {
+    $("span").removeClass('negative');
+    }
+  });
+})
