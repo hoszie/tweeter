bba1ae4e8af37263d42fff1234b76b057a567262
Nick <nhoszko@gmail.com>
2019-11-06 04:18:48 +0000

    jQuery that hides and shows the handle

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
-  $("textarea").on('keyup', function() {
-    $(this).parent().find("span").text(140 - $(this).val().length);
+  $('textarea').on('keyup', function() {
+    $(this).parent().find('span').text(140 - $(this).val().length);
     let $tweetLength = $(this).val().length;
     if ($tweetLength > 140) {
-      $("span").addClass('negative');
+      $('span').addClass('negative');
     } else {
-    $("span").removeClass('negative');
+    $('span').removeClass('negative');
     }
+  });
+  $('#handle').hide();
+  $('.posted-tweet').mouseenter(function() {
+    $('#handle').show();
+  }).mouseleave(function() {
+    $('#handle').hide();
   });
 })
