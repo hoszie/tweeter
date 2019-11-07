67125e212da036875374c343e50979f80d29663c
Nick <nhoszko@gmail.com>
2019-11-07 22:58:40 +0000

    Refactored createTweetElement function to be safer using jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,25 +3,33 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const createTweetElement = function(tweetObj) {
-  const $tweet = `
+const createTweetElement = function(tweetObj)
+{
+  const $img = $('<img>').attr('src', tweetObj.user.avatars);
+  const $name = $('<span>').addClass('name').text(tweetObj.user.name);
+  const $handle = $('<span>').addClass('handle').text(tweetObj.user.handle);
+  const $header = $('<div>').addClass('id-tweet');
   
-    <article class="tweet">
-      <div class="id-tweet">
-        <img src='${tweetObj.user.avatars}'> 
-        <span class="avatar">${tweetObj.user.name}</span>
-        <span class="handle">${tweetObj.user.handle}</span>
-      </div>
-      <div id="content-tweet">
-          <p class="tweet-words">${tweetObj.content.text}</p>
-          <footer class="bottom">
-            <span class="timestamp">${tweetObj.created_at}</span>
-            <span class="likes"><i class="fa fa-flag-o"></i><i class="fa fa-retweet"></i><i class="fa fa-heart-o"></i></span>
-          </footer>
-      </div>
-    </article>
-  `;
-  return $tweet;
+  $header.append($img, $name, $handle);
+  
+  const $p = $('<p>').addClass('tweet-words').text(tweetObj.content.text);
+  const $timestamp = $('<span>').addClass('timestamp').text(tweetObj.created_at);
+  const $icons = $('<span>').addClass('likes');
+  const $footer = $('<footer>').addClass('bottom');
+  const $contentTweet = $('<div>').addClass('content-tweet');
+  const $iFlagO = $('<i>').addClass('fa fa-flag-o');
+  const $iFaRetweet = $('<i>').addClass('fa fa-retweet');
+  const $iFaHeart = $('<i>').addClass('fa fa-heart-o');
+
+
+  $icons.append($iFlagO, $iFaRetweet, $iFaHeart);
+  $footer.append($timestamp, $icons);
+  $contentTweet.append($p, $footer);
+
+  const $article = $('<article>').addClass('tweet');
+
+  $article.append($header, $contentTweet);
+  return $article;
 }
 
 const renderTweets = function (data) {
