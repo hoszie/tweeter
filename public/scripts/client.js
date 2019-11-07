1e53e04585453977e2d206512d762d2481b8a511
Nick <nhoszko@gmail.com>
2019-11-07 02:31:59 +0000

    Rendering template with createTweetElement function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,61 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = function(tweetObj) {
+  const $tweet = `
+  <section class='tweets-container'>
+  <article class="tweet">
+    <div class="id-tweet">
+      <span id="avatar">${tweetObj.user.avatars} ${tweetObj.user.name}</span>
+      <span class="handle">${tweetObj.user.handle}</span>
+    </div>
+    <div id="content-tweet">
+        <p class="tweet-words">${tweetObj.content.text}</p>
+        <footer class="bottom">
+          <span class="timestamp">${tweetObj.created_at}</span>
+          <span class="likes"><i class="fa fa-flag-o"></i><i class="fa fa-retweet"></i><i class="fa fa-heart-o"></i></span>
+        </footer>
+    </div>
+  </article>
+</section>
+  `;
+  return $tweet;
+}
+const dummyTweets = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const firstTweet = dummyTweets[0];
+
+const $tweet = createTweetElement(firstTweet);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('.tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+// $(".tweets-container").append(crap)
+const secondTweet = dummyTweets[1];
+const $tweet2 = createTweetElement(secondTweet);
+$('.tweets-container').append($tweet2);
