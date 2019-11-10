5e37172d5945131e81eb664cd98c972d3f465516
Nick <nhoszko@gmail.com>
2019-11-10 00:29:23 +0000

    Removed comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
 
   const fixNewTweetCounterColor = function() {
-    // JH sez: I feel like your selectors here ('span' and ('textarea') feel a bit too broad, though of course it's totally working okay for now
     $(this).parent().find('span').text(140 - $(this).val().length);
     let $tweetLength = $(this).val().length;
     if ($tweetLength > 140) {
-      // JH says: reselecting $('span') here is not ideal
       $('span').addClass('negative');
     } else {
-      // JH says: reselecting $('span') here is not ideal
       $('span').removeClass('negative');
     }
   };
